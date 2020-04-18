e611f5730a38775c68cca012706521a09a2c53bc
Victor Wiedemann <victor.wiedemann@gmail.com>
2020-04-18 13:33:08 -0700

    committing section 2 programs

--- firstClass/main.py
+++ firstClass/main.py
@@ -1,1 +1,40 @@
 print ("hello world")
+print ('what is your name')
+#myName = input()
+#if myName:
+#    print ('hello you')
+#else:
+#    print('what are you')
+
+
+#print ('It is good to meet you, ' + myName)
+#print ('The length of your name is: ' + str(len(myName)))
+#print ('What is your age?')
+#myAge = input()
+#print('You will be ' + str(int(myAge) + 1) + ' in a year') 
+
+#
+# # spam = ''
+# while spam == '':
+#     print ('please type your name')
+#     spam = input()
+#
+
+# spam = 0
+# while spam < 5:
+#     spam += 1
+#     if spam == 4:
+#         continue
+#     print (spam)
+
+#arr = range(5)
+#inputNum = input()
+#total = 0
+#for i in range(int(inputNum)):
+#    total += i + 1
+#    print('rolling total: ' + str(total))
+#print('total: ' + str(total))
+
+
+for i in range(0, 10, -1):
+    print (str(i))
